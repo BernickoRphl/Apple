d4bd37eef1cdd34c357f30d15e36be2a62a07d01
Nathan Darrell <ndsutejo@gmail.com>
2024-05-11 21:10:49 +0700

    p

--- Brush Buddies/Brush Buddies/ContentView.swift
+++ Brush Buddies/Brush Buddies/ContentView.swift
@@ -1,20 +1,28 @@
-//
-//  ContentView.swift
-//  Brush Buddies
-//
-//  Created by MacBook Pro on 11/05/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color.blue
-                .ignoresSafeArea()
-            Text("Main Page")
-                .foregroundColor(.white)
-                .font(.system(size:30))
+        let streak = 10
+        VStack {
+            GroupBox {
+                if streak > 7 {
+                    Text("Current Streak Is \(streak)!\nKeep Up The Good Work!")
+                } else {
+                    Text("Current Streak is \(streak)!\nLet's Get Brushing")
+                }
+            } label: {
+                Label("Streak", systemImage: "flame.fill")
+                    .foregroundColor(.red)
+            }
+            .frame(width: 350)
+            NavigationView {
+                NavigationLink(destination: TimerView()) {
+                    Image(systemName: "livephoto.play")
+                        .font(.system(size: 120))
+                        .foregroundColor(.red)
+                }
+            }
+            Spacer()
         }
     }
 }
