6725b0e062f027a7b757bd162581e5eb92cf8c44
Nathan Darrell <ndsutejo@gmail.com>
2024-05-11 07:53:18 +0700

    Initial

---
+++ Brush Buddies/Brush Buddies/Brush_BuddiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Brush_BuddiesApp.swift
+//  Brush Buddies
+//
+//  Created by MacBook Pro on 11/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Brush_BuddiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
