2151cb83de38d6ec72c7aaf08b61586850cde6d3
Lie, Samuel Miracle Kristanto <114904516+AttraqtiA@users.noreply.github.com>
2024-05-15 18:33:24 +0700

    SplashScreen + NewSprout Page Done

--- BloomSketch/BloomSketch/NewSproutView.swift
+++ BloomSketch/BloomSketch/NewSproutView.swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct TimerView: View {
+struct NewSproutView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    TimerView()
+    NewSproutView()
 }
--- BloomSketch/BloomSketch/SplashScreenView.swift
+++ BloomSketch/BloomSketch/SplashScreenView.swift
@@ -11,32 +11,49 @@ struct SplashScreenView: View {
     @State private var isActive = false
     @State private var size = 0.8
     @State private var opacity = 0.5
+    
     var body: some View {
-        if isActive{
-            ContentView()
-        } else{
-            VStack{
-                VStack{
-                    Image(systemName: "hare.fill")
-                        .font(.system(size: 80))
-                        .foregroundColor(.red)
-                    Text("Brush Buddies")
-                        .font(Font.custom("Baskerville-Bold", size: 26))
-                        .foregroundColor(.black.opacity(0.8))
-                }
-                .scaleEffect(size)
-                .opacity(opacity)
-                .onAppear(){
-                    withAnimation(.easeIn(duration: 1.2)){
-                        self.size = 0.9
-                        self.opacity = 1.0
+        ZStack {
+            Color(#colorLiteral(red: 0.8352941176, green: 0.8941176471, blue: 0.8666666667, alpha: 1))
+                .ignoresSafeArea() // untuk include top and bottom part (safe area)
+            
+            if isActive {
+                ContentView()
+            } else {
+                VStack {
+                    Spacer()
+                    
+                    VStack {
+                        Image("BloomSketch_LogoWhite")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 240, height: 240)
+                            .foregroundColor(.red)
+                        
+                        Text("BloomSketch")
+                            .font(Font.custom("Baskerville-Bold", size: 40))
+                            .foregroundColor(Color(hex: 0x1B3F2E))
                     }
+                    .scaleEffect(size)
+                    .opacity(opacity)
+                    .onAppear {
+                        withAnimation(.easeIn(duration: 1.2)) {
+                            self.size = 0.9
+                            self.opacity = 1.0
+                        }
+                    }
+                    
+                    Spacer()
+                    
+                    Image("BottomBarLeaves_Crop")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
                 }
-            }
-            .onAppear {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0){
-                    withAnimation{
-                        self.isActive = true
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                        withAnimation {
+                            self.isActive = true
+                        }
                     }
                 }
             }
